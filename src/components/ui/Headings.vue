6820f46275db377343ceb58f96b0588e33354685
ph-code-challenges <eu@rjper.dev>
2024-01-11 16:51:55 -0300

    feat(step1): added some ui components

---
+++ src/components/ui/Headings.vue
@@ -0,0 +1,33 @@
+<template>
+  <component
+    v-bind:is="props.size"
+    v-bind:class="`${
+      FONT_SIZE_DICTIONARY[props.size]
+    } text-green-200 bg-red-200`"
+  >
+    <slot />
+  </component>
+</template>
+
+<script setup>
+import { defineProps } from 'vue'
+
+const props = defineProps({
+  size: {
+    type: String,
+    default: 'h1',
+    validator: (value) => {
+      const ACCEPTED_VALUES = ['h1', 'h2', 'h3', 'h4', 'h5']
+      return ACCEPTED_VALUES.includes(value)
+    },
+  },
+})
+
+const FONT_SIZE_DICTIONARY = {
+  h1: 'text-2xl',
+  h2: 'text-xl',
+  h3: 'text-lg',
+  h4: 'text-base',
+  h5: 'text-sm',
+}
+</script>
